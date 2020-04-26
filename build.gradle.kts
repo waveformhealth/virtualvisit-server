dc2b1915bf97dc01ee1a7241749ee7f1356d1f7f
Trevor Halvorson <trevor.halvorson@gmail.com>
2020-04-26 11:54:54 -0700

    Add support for deployment via Docker container
    We package our app as a JAR file to be ran in a Docker container.
    
    See: https://ktor.io/quickstart/quickstart/docker.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,33 @@
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val twilio_version: String by project
 
+buildscript {
+    repositories {
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+    }
+
+    dependencies {
+        classpath("com.github.jengelman.gradle.plugins:shadow:5.2.0")
+    }
+}
+
 plugins {
     application
     kotlin("jvm") version "1.3.70"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
 group = "com.waveform.virtualvisit"
 version = "0.0.1"
 
 application {
-    mainClassName = "io.ktor.server.netty.EngineMain"
+    mainClassName = "com.waveform.virtualvisit.ApplicationKt"
 }
 
 repositories {
@@ -40,3 +52,12 @@ kotlin.sourceSets["test"].kotlin.srcDirs("test")
 
 sourceSets["main"].resources.srcDirs("resources")
 sourceSets["test"].resources.srcDirs("testresources")
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("shadow")
+        archiveClassifier.set("")
+        archiveVersion.set("")
+        mergeServiceFiles()
+    }
+}
